47e5d8f9c8f1e6c771406eb30661f41d8f06a0fb
siphoPrince <mabirimisesipho@gmail.com>
2024-03-14 04:46:36 -0700

    fix pycode

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -4,6 +4,7 @@
 
 import math
 
+
 def floor(n: float) -> int:
     """Return the floor of a float."""
     return math.floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""conc to strings"""
+
+def to_str(n: float) -> str:
+    """Return the string representation of a float."""
+    return str(n)
