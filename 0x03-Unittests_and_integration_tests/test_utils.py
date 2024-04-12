6d5f9d3226a1c3b7d226d7ab041e2315601f4ffc
siphoprince <mabirimisesipho@gmail.com>
2024-04-12 04:13:09 -0700

    test util

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,6 +14,8 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
 if __name__ == '__main__':
